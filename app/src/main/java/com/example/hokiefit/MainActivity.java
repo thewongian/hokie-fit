298292464a254523758a3fb32fd89b685e6f464b
LiamMcFadden <liamm18@vt.edu>
2021-04-27 10:37:26 -0400

    frag view/styling mostly done

--- app/src/main/java/com/example/hokiefit/MainActivity.java
+++ app/src/main/java/com/example/hokiefit/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.hokiefit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Button to display fragment expansion functionality
+         * TODO: Remove button from this class and XML
+         * TODO: Set animation for opening the fragment
+         */
+        Button temp = findViewById(R.id.expand_settings_frag);
+        ConstraintLayout mainLayout = findViewById(R.id.main_layout);
+        // set up settings fragment
+        SettingsFragment settingsFragment = new SettingsFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, settingsFragment)
+                .hide(settingsFragment)
+                .commit();
+
+        View.OnClickListener ClickListener = v -> {
+            /*
+             * display the fragment on button click (no toggle functionality)
+             */
+            if (v.getId() == temp.getId()) {
+                getSupportFragmentManager().beginTransaction()
+                        .show(settingsFragment)
+                        .commit();
+            }
+            if (v.getId() == mainLayout.getId() && v.getId() != settingsFragment.getId()) {
+                getSupportFragmentManager().beginTransaction()
+                        .hide(settingsFragment)
+                        .commit();
+            }
+        };
+
+        temp.setOnClickListener(ClickListener);
+        mainLayout.setOnClickListener(ClickListener);
     }
 }
